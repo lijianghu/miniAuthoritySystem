8e2333b835f538ea55c04aec3090bcaa0d8a9710
lijianghu <13511079747@163.com>
2017-11-24 10:42:22 +0800

    增加doc

--- migo-web/src/main/java/com/migo/aop/SysLogAspect.java
+++ migo-web/src/main/java/com/migo/aop/SysLogAspect.java
@@ -17,13 +17,11 @@
 
 package com.migo.aop;
 
-import com.alibaba.fastjson.JSON;
-import com.migo.annotation.SysLog;
-import com.migo.entity.SysLogEntity;
-import com.migo.service.SysLogService;
-import com.migo.utils.HttpContextUtils;
-import com.migo.utils.IPUtils;
-import com.migo.utils.ShiroUtils;
+import java.lang.reflect.Method;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -32,9 +30,13 @@ import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.http.HttpServletRequest;
-import java.lang.reflect.Method;
-import java.util.Date;
+import com.alibaba.fastjson.JSON;
+import com.migo.annotation.SysLog;
+import com.migo.entity.SysLogEntity;
+import com.migo.service.SysLogService;
+import com.migo.utils.HttpContextUtils;
+import com.migo.utils.IPUtils;
+import com.migo.utils.ShiroUtils;
 
 /**
  * @author 知秋
@@ -43,47 +45,47 @@ import java.util.Date;
 @Aspect
 @Component
 public class SysLogAspect {
-    @Autowired
-    private SysLogService sysLogService;
+  @Autowired
+  private SysLogService sysLogService;
 
-    @Pointcut("@annotation(com.migo.annotation.SysLog)")
-    public void logPointCut() {
+  @Pointcut("@annotation(com.migo.annotation.SysLog)")
+  public void logPointCut() {
 
+  }
+
+  @Before("logPointCut()")
+  public void saveSysLog(JoinPoint joinPoint) {
+    MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+    Method method = signature.getMethod();
+
+    SysLogEntity sysLog = new SysLogEntity();
+    SysLog syslog = method.getAnnotation(SysLog.class);
+    if (syslog != null) {
+      // 注解上的描述
+      sysLog.setOperation(syslog.value());
     }
 
-    @Before("logPointCut()")
-    public void saveSysLog(JoinPoint joinPoint) {
-        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-        Method method = signature.getMethod();
+    // 请求的方法名
+    String className = joinPoint.getTarget().getClass().getName();
+    String methodName = signature.getName();
+    sysLog.setMethod(className + "." + methodName + "()");
 
-        SysLogEntity sysLog = new SysLogEntity();
-        SysLog syslog = method.getAnnotation(SysLog.class);
-        if(syslog != null){
-            //注解上的描述
-            sysLog.setOperation(syslog.value());
-        }
+    // 请求的参数
+    Object[] args = joinPoint.getArgs();
+    String params = JSON.toJSONString(args[0]);
+    sysLog.setParams(params);
 
-        //请求的方法名
-        String className = joinPoint.getTarget().getClass().getName();
-        String methodName = signature.getName();
-        sysLog.setMethod(className + "." + methodName + "()");
+    // 获取request
+    HttpServletRequest request = HttpContextUtils.getHttpServletRequest();
+    // 设置IP地址
+    sysLog.setIp(IPUtils.getIpAddr(request));
 
-        //请求的参数
-        Object[] args = joinPoint.getArgs();
-        String params = JSON.toJSONString(args[0]);
-        sysLog.setParams(params);
+    // 用户名
+    String username = ShiroUtils.getUserEntity().getUsername();
+    sysLog.setUsername(username);
 
-        //获取request
-        HttpServletRequest request = HttpContextUtils.getHttpServletRequest();
-        //设置IP地址
-        sysLog.setIp(IPUtils.getIpAddr(request));
-
-        //用户名
-        String username = ShiroUtils.getUserEntity().getUsername();
-        sysLog.setUsername(username);
-
-        sysLog.setCreateDate(new Date());
-        //保存系统日志
-        sysLogService.save(sysLog);
-    }
+    sysLog.setCreateDate(new Date());
+    // 保存系统日志
+    sysLogService.save(sysLog);
+  }
 }
